ebbc80f4029601dfe1ab2340728b1103c53ec7ff
vanes430 <vanessimbolon2020@gmail.com>
2026-07-03 22:15:51 +0700

    chore: update GenerateStatics.sh to use bun

--- WAProto/GenerateStatics.sh
+++ WAProto/GenerateStatics.sh
@@ -1,4 +1,4 @@
-yarn pbjs -t static-module -w commonjs -o ./WAProto/index.js ./WAProto/WAProto.proto;
-yarn pbts -o ./WAProto/index.d.ts ./WAProto/index.js;
-
-#protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_opt=env=node,useOptionals=true,forceLong=long --ts_proto_out=. ./src/Binary/WAMessage.proto;
+#!/bin/bash
+# Kompilasi WAProto.proto menjadi berkas JavaScript dan tipe TypeScript menggunakan Bun
+bunx pbjs -t static-module -w commonjs -o ./WAProto/index.js ./WAProto/WAProto.proto
+bunx pbts -o ./WAProto/index.d.ts ./WAProto/index.js
